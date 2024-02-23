4e4d899567fcd7b0772effe3a07801988794a588
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-02-23 14:31:16 +0530

    strings

--- 01_basics/strings.py
+++ 01_basics/strings.py
@@ -10,7 +10,7 @@
 
 # print(chai[-1])
 
-num_list = "0123456789"
+# num_list = "0123456789"
 # print(num_list[-10])
 # print(num_list[:])
 # print(num_list[3:])
@@ -21,10 +21,55 @@ num_list = "0123456789"
 # print(chai.lower())
 # print(chai.upper())
 
-chai2 = "    Masala Chai     "
+# chai2 = "    Masala Chai     "
 # print(chai2)
 # print(chai2.strip())
 
-chai3 = "Lemon Tea"
-print(chai3.replace("Lemon", "Ginger"))
+# chai3 = "Lemon Tea"
+# print(chai3.replace("Lemon", "Ginger"))
 
+# chai = "Lemon, Ginger, Masala, Milk"
+# print(chai.split())
+# print(chai.split(", "))
+
+chai1 = "Ginger Chai"
+# print(chai1.find("Chai"))
+# print(chai1.find("chai"))     // returns -1 if it was not included
+
+# chai = "Masala Chai Chai Chai"
+# print(chai.count("Chai"))
+
+iceCream_type = "Stawberry"
+quantity = 2
+order = "I ordered {} cups of {} flavour ice-cream"
+# print(order)
+# print(order.format(quantity, iceCream_type))
+
+chai_variety = ["Lemon", "Masala", "Ginger"]
+# print(chai_variety)
+# print("-".join(chai_variety))
+# print(" ".join(chai_variety))
+# print(", ".join(chai_variety))
+
+# chai = "Masala Chai"
+# print(len(chai))
+# for letter in chai:
+#     print(letter)
+
+boy = "He said \"Masala chai was awesome\" "
+print(boy)
+
+# chai = "Masala \nChai"
+# print(chai)
+
+# chai2 = r"Masala \nChai"
+# print(chai2)
+
+# chai = r"c:\user\pwd"
+# print(chai)
+
+paani = "c:\\user\\pwd"
+print(paani)
+
+chai = "Masala Chai"
+print("Masala" in chai)
