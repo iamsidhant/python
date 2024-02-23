fb016a41bc0f504a8bac52df14ed8fb53ca39f31
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-02-23 13:30:02 +0530

    python

---
+++ 01_basics/numbers.py
@@ -0,0 +1,25 @@
+import math
+
+# print(math.trunc(2.6))
+# print(math.ceil(2.9))
+# print(math.floor(2.5))
+
+# n = -5
+# print(bin(n))
+
+# print(oct(123))
+# print(hex(64))
+# print(3 ** 100)
+# print(pow(3, 100))
+
+# print(complex(5, 3))
+
+tax = 12.5 / 100
+price = 1008.263
+# print(price * tax)
+cost = price + (price * tax)
+# print(cost)
+
+# print(round(cost, 2))
+
+print((3 * "bye") + "Goodbye")
---
+++ 01_basics/strings.py
@@ -0,0 +1,30 @@
+
+# print("lap" + "top")
+# print("soft" + "ware" + " eng" + "ineer")
+
+# chai = "Masala Chai"
+# print(chai[0])
+
+# slice_chai = chai[0:6]
+# print(slice_chai)
+
+# print(chai[-1])
+
+num_list = "0123456789"
+# print(num_list[-10])
+# print(num_list[:])
+# print(num_list[3:])
+# print(num_list[:7])
+# print(num_list[0:7:2])
+
+# print(chai)
+# print(chai.lower())
+# print(chai.upper())
+
+chai2 = "    Masala Chai     "
+# print(chai2)
+# print(chai2.strip())
+
+chai3 = "Lemon Tea"
+print(chai3.replace("Lemon", "Ginger"))
+
