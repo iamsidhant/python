5a6d4f7ca1b3edf48d08561fdb31b442167d9a1b
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-02-24 11:33:09 +0530

    tuples

---
+++ 01_basics/tuples.py
@@ -0,0 +1,26 @@
+tea_types = ("Black", "Green", "Oolong")
+# print(tea_types)
+# print(tea_types[1])
+# print(tea_types[-1])
+# print(tea_types[1:])
+
+# tea_types[1] = "Milk"     # tuples are immutable
+
+# print(len(tea_types))
+
+# more_tea = ("Herbal", "Earl Grey")
+# all_tea = tea_types + more_tea
+# print(all_tea)
+
+# if "Green" in all_tea:
+#     print("I have Green tea")
+
+more_tea = ("Herbal", "Earl Grey", "Herbal")
+# print(more_tea.count("Herba"))
+# print(more_tea.count("Herbal"))
+
+# print(tea_types)
+(black, green, oolong) = tea_types
+# print(black)
+
+print(type(tea_types))
