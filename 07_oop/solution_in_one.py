0472efd391c8c1ea3c60a98a233d8675e82f69d0
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-02-27 11:46:41 +0530

    oop in python

--- 07_oop/solution_in_one.py
+++ 07_oop/solution_in_one.py
@@ -4,17 +4,25 @@ class Car:
 
     def __init__(self, brand, model):
         self.__brand = brand
-        self.model = model
+        self.__model = model
         Car.total_car += 1
 
     def get_brand(self):
         return self.__brand + " !"    
 
     def full_name(self):
-        return f"{self.__brand} {self.model}"  
+        return f"{self.__brand} {self.__model}"  
 
     def fuel_type(self):
-        return "Petrol or Diesel"  
+        return "Petrol or Diesel"
+
+    @staticmethod
+    def general_description():
+        return "Cars are means of transport"  
+    
+    @property
+    def model(self):
+        return self.__model
 
 
 class ElectricCar(Car):
@@ -27,16 +35,24 @@ class ElectricCar(Car):
     
 
 # my_tesla = ElectricCar("Tesla", "Model S", "85kWh")
+# print(isinstance(my_tesla, Car))
+# print(isinstance(my_tesla, ElectricCar))
+
+
 # print(my_tesla.__brand)               # made private, can be accessed by getter method
 # print(my_tesla.get_brand())
 # print(my_tesla.fuel_type())
 
 
-my_car = Car("Tata", "Safari")
-my_car_3 = Car("Tata", "Nexon")
+# my_car = Car("Tata", "Safari")
+# my_car.model = "City"
+# my_car_3 = Car("Tata", "Nexon")
 # print(my_car.fuel_type())
 
-print(Car.total_car)
+# print(my_car.general_description())
+# print(my_car.model)
+# print(Car.total_car)
+
 
 # my_car = Car("Toyota", "Camry")        
 # print(my_car.brand)
@@ -45,3 +61,21 @@ print(Car.total_car)
 
 # my_new_car = Car("Tata", "Safari")        
 # print(my_new_car.model)
+
+
+
+class Battery:
+    def battery_info(self):
+        return "this is battery"
+    
+class Engine:
+    def engine_info(self):
+        return "this is engine"
+
+class ElectricCarTwo(Battery, Engine, Car):
+    pass
+
+my_new_tesla = ElectricCarTwo("Tesla", "Model S")
+
+print(my_new_tesla.battery_info())
+print(my_new_tesla.engine_info())
