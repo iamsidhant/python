dc3fefe54baef8998e34794cf9841cdb5e46cf4e
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-03-11 14:16:43 +0530

    project with python and mongodb

--- 12_python_mongodb/youtube_manager_mongodb.py
+++ 12_python_mongodb/youtube_manager_mongodb.py
@@ -1,22 +1,30 @@
 
 from pymongo import MongoClient
+from bson import ObjectId
 
-client = MongoClient("mongodb+srv://<username>:<password>@cluster0.kcq4zop.mongodb.net/")
+client = MongoClient("mongodb+srv://<username>:<password>@cluster0.kcq4zop.mongodb.net/", tlsAllowInvalidCertificates=True)
+# Not a good idea to include id and password in code files
+# tlsAllowInvalidCertificates=True - Not a good way to handle ssl 
 
 print(client)
 
+db = client["ytmanager"]
+video_collection = db["videos"]
 
-def add_videos():
-    pass
+# print(video_collection)
+
+def add_videos(name, time):
+    video_collection.insert_one({"name": name, "time": time})
 
 def list_videos():
-    pass
+    for video in video_collection.find():
+        print(f"ID: {video['_id']}, Name: {video['name']} , Time: {video['time']}")
 
-def update_video():
-    pass
+def update_video(video_id, new_name, new_time):
+    video_collection.update_one({"_id": ObjectId[video_id]}, {"$set": {"name": new_name, "time": new_time}})
 
-def delete_video():
-    pass
+def delete_video(video_id):
+    video_collection.delete_one({"_id": ObjectId[video_id]})
 
 
 def main():
@@ -37,9 +45,9 @@ def main():
             add_videos(name, time)
         elif choice == '3':
             video_id = input("Enter the video id to update: ")
-            name = input("Enter the video name: ")
-            time = input("Enter the video time: ")
-            update_video(video_id, name, time)
+            new_name = input("Enter the updated video name: ")
+            new_time = input("Enter the updated video time: ")
+            update_video(video_id, new_name, new_time)
         elif choice == '4':
             video_id = input("Enter the video id to update: ")
             delete_video(video_id)
